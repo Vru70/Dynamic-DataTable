4c8b17cee809654f155c6b2cee866047db3a909d
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-06-02 18:18:00 +0530

    child paginator

--- force-app/main/default/lwc/paginator/paginator.js
+++ force-app/main/default/lwc/paginator/paginator.js
@@ -1,3 +1,22 @@
+/**
+ * @author            : Vrushabh Uprikar
+ * @last modified on  : 06-02-2021
+ * @last modified by  : Vrushabh Uprikar
+ * Modifications Log 
+ * Ver   Date         Author             Modification
+ * 1.0   06-02-2021   Vrushabh Uprikar   Initial Version
+**/
 import { LightningElement } from 'lwc';
 
-export default class Paginator extends LightningElement {}
+export default class Paginator extends LightningElement
+{
+    previousHandler()
+    {
+        this.dispatchEvent(new CustomEvent('previous'));
+    }
+
+    nextHandler()
+    {
+        this.dispatchEvent(new CustomEvent('next'));
+    }
+ }
