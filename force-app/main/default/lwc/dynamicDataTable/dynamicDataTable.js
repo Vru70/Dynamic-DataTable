b9c919ef26ee0b013c300f8e56eecee19c910ffa
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-06-04 17:05:21 +0530

    sorted

--- force-app/main/default/lwc/dynamicDataTable/dynamicDataTable.js
+++ force-app/main/default/lwc/dynamicDataTable/dynamicDataTable.js
@@ -9,7 +9,8 @@
 import { LightningElement, api, track } from 'lwc';
 import getFieldsAndRecords from '@salesforce/apex/FieldSetHelper.getFieldsAndRecords';
 let i = 0; // Counter
-export default class DynamicDataTable extends LightningElement {
+export default class DynamicDataTable extends LightningElement
+{
     @api recordId;  // record id from record detail page e.g. ''0012v00002WCUdxAAH'
     @api SFDCobjectApiName; //kind of related list object API Name e.g. 'Account'
     @api fieldSetName; // FieldSet which is defined on that above object e.g. 'AccFieldSet'
@@ -28,7 +29,7 @@ export default class DynamicDataTable extends LightningElement {
     @track totalPage = 0; //total number of page is needed to display all records
     @track dataToDisp = []; //data to be displayed in the table
 
-    
+
     @track totalSelection;
     @track hasPageChanged;
     @track initialLoad = true;
@@ -41,54 +42,53 @@ export default class DynamicDataTable extends LightningElement {
         getFieldsAndRecords({
             strObjectApiName: this.SFDCobjectApiName,
             strfieldSetName: this.fieldSetName
-            })
-            .then(data => {
-                //get the entire map
-                let objStr = JSON.parse(data);
+        })
+        .then(data => {
+            //get the entire map
+            let objStr = JSON.parse(data);
+            /* retrieve listOfFields from the map,
+            here order is reverse of the way it has been inserted in the map */
+            let listOfFields = JSON.parse(Object.values(objStr)[1]);
 
-                /* retrieve listOfFields from the map,
-                 here order is reverse of the way it has been inserted in the map */
-                let listOfFields = JSON.parse(Object.values(objStr)[1]);
+            //retrieve listOfRecords from the map
+            let listOfRecords = JSON.parse(Object.values(objStr)[0]);
 
-                //retrieve listOfRecords from the map
-                let listOfRecords = JSON.parse(Object.values(objStr)[0]);
+            let items = []; //local array to prepare columns
 
-                let items = []; //local array to prepare columns
+            listOfFields.map(element => {
 
-                listOfFields.map(element => {
-                    
-                        items = [...items, {
-                            label: element.label,
-                            fieldName: element.fieldPath
-                        }];
-                });
+                items = [...items, {
+                label: element.label,
+                    fieldName: element.fieldPath
+                }];
+            });
 
-                //finally assigns item array to columns
-                this.columns = items;
-                var xx = JSON.stringify(listOfRecords);
-                this.allData = JSON.parse(xx);
-                console.log('this.allData:', this.allData); // collecting all data
-
-                this.totalRecountCount = listOfRecords.length;
-                console.log('totalRecountCount >', this.totalRecountCount); //here it is 10   
-
-                this.totalPage = Math.ceil(this.totalRecountCount / this.pageSize); //here it is 5
-                console.log('totalPage >', this.totalPage);
-
-                this.dataToDisp = this.allData.slice(0, this.pageSize);
-                console.log('this.dataToDisp', this.dataToDisp);
-                this.endingRecord = this.pageSize;
-
-                this.lblobjectName = this.SFDCobjectApiName; // Assigning Headder i.e Acount
-                this.error = undefined;
-            })
-            .catch(error => {
-                this.error = error;
-                console.log('error', error);
-                this.allData = undefined;
-                this.lblobjectName = this.SFDCobjectApiName;
-            })
+            //finally assigns item array to columns
+            var xx = JSON.stringify(listOfRecords);
+            this.allData = JSON.parse(xx);
+            console.log('this.allData:', this.allData); // collecting all data
+            this.columns = items;
+            this.totalRecountCount = listOfRecords.length;
+            console.log('totalRecountCount >', this.totalRecountCount); //here it is 10   
+            
+            this.totalPage = Math.ceil(this.totalRecountCount / this.pageSize); //here it is 5
+            console.log('totalPage >', this.totalPage);
+            
+            this.dataToDisp = this.allData.slice(0, this.pageSize);
+            console.log('this.dataToDisp', this.dataToDisp);
+            this.endingRecord = this.pageSize;
+            
+            this.lblobjectName = this.SFDCobjectApiName; // Assigning Headder i.e Acount
+            this.error = undefined;
+        })
+        .catch(error => {
+            this.error = error;
+            console.log('error', error);
+            this.allData = undefined;
+            this.lblobjectName = this.SFDCobjectApiName;
+        })
     }
+    
 
     //this method displays records page by page
     displayRecordPerPage(page)
@@ -107,7 +107,6 @@ export default class DynamicDataTable extends LightningElement {
     //clicking on previous button this method will be called
     previousHandler()
     {
-        
         if (this.page > 1)
         {
             this.page = this.page - 1; //decrease page by 1
@@ -115,22 +114,22 @@ export default class DynamicDataTable extends LightningElement {
             this.hasPageChanged = true;
             this.template.querySelector('lightning-datatable').selectedRows = this.setData[this.page];
             console.log('Inside pre:', JSON.stringify(this.setData[this.page]));
-            
-        }  
+
+        }
     }
 
     //clicking on next button this method will be called
     nextHandler()
-    {   
+    {
         if ((this.page < this.totalPage) && this.page !== this.totalPage)
-        {            
+        {
             this.page = this.page + 1; //increase page by 1
             console.log('crpage:', this.page);
             this.displayRecordPerPage(this.page);
             this.hasPageChanged = true;
             this.template.querySelector('lightning-datatable').selectedRows = this.setData[this.page];
             console.log('Inside Next:', JSON.stringify(this.setData[this.page]));
-        }  
+        }
     }
 
     handleRowSelection(event)
@@ -139,5 +138,4 @@ export default class DynamicDataTable extends LightningElement {
         console.log('selectedRows:', JSON.stringify(this.selectedRows));
         this.setData[this.page] = this.selectedRows;
     }
-    
 }
